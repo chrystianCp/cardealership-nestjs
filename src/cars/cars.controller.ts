411eb4f7145599d75705bce336c6e8f79cbe8d53
chrystianCp <chrystian.cardenas@outlook.com>
2022-09-28 12:48:11 -0500

    Feat(Cars):controller, service, dtos

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -4,36 +4,43 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
   ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
+
   @Get()
   getAllCars() {
     return this.carsService.findAll();
   }
+
   @Get(':id')
   getCarById(@Param('id', ParseUUIDPipe) id: string) {
     return this.carsService.findOneById(id);
   }
+
   @Post()
-  createCar(@Body() body: CreateCarDto) {
-    console.log('hola');
-    return body;
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return this.carsService.create(createCarDto);
   }
+
   @Patch(':id')
-  updateCar(@Body() body: any) {
-    return body;
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto,
+  ) {
+    return this.carsService.update(id, updateCarDto);
   }
+
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: string) {
-    return { id };
+  deleteCar(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.delete(id);
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,5 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dto';
 import { Car } from './interfaces/car.interface';
 
 @Injectable()
@@ -21,12 +26,44 @@ export class CarsService {
       model: 'TrackHawk',
     },
   ];
+
   findAll() {
     return this.cars;
   }
+
   findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id: ${id} not found.`);
     return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+    this.cars.push(car);
+    return car;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDb = this.findOneById(id);
+
+    if (updateCarDto.id && updateCarDto.id !== id)
+      throw new BadRequestException(`Car Id is not valid inside body`);
+
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDb = { ...carDb, ...updateCarDto, id };
+        return carDb;
+      }
+      return car;
+    });
+    return carDb;
+  }
+
+  delete(id: string) {
+    const carDb = this.findOneById(id);
+    this.cars = this.cars.filter((car) => car.id !== carDb.id);
   }
 }
